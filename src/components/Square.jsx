e0749ebdde71cc49d20fa8434a7433818b5e6c7a
AnujIstwal <istwalbimal@gmail.com>
2022-06-24 16:15:22 +0530

    init commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Square({ value, onClick, isWinningSquare }) {
+  return (
+    <button
+      type="button"
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+      onClick={onClick}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default Square;
